0dd3636a288f24aa529bd540330e5eb6628c8b4f
yltl <t16586yt@sfc.keio.ac.jp>
2017-04-27 12:44:37 +0900

    表か裏かマン勝つコードを追加

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,16 @@
+  const GU = 0;
+  const CHOKI = 1;
+  const PA = 2;
+
+let matches = 0;
+
 function action(oppornent){
-  return 1;
+  matches = matches + 1;
+  let amari = matches % 2;
+  let isEven = amari == 0;
+  console.log(`${matches}%2 = ${amari}: ${isEven}`);
+  if(isEven){
+    return GU
+  }
+  return PA;
 }
