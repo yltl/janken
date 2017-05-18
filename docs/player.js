f8697a12a522e81671d922dcf6d6b09416c3e4ec
yltl <t16586yt@sfc.keio.ac.jp>
2017-05-18 12:34:08 +0900

    順番に出していくマンに勝つ/相手によって出す手を変える

--- docs/player.js
+++ docs/player.js
@@ -4,10 +4,28 @@
 
 let matches = 0;
 
+function countup(){
+matches = matches + 1;
+return matches;
+}
 
+//　チョキ大好きマンに対するアクション
+function actionAgainstChokiLove(){
+  return GU;
+}
 
+//　表か裏かマンに対するアクション
+function actionAgainstOmoteUra(){
+  const isEven = matches % 2 == 0;
+  let result = PA;
+  if (isEven) {
+    result = GU;
+  }
+  return result;
+}
+
+//　帰ってきた表か裏かマンに対するアクション
 function actionAgainstOmoteUra2(){
-    matches = matches + 1;
   let amari = matches % 3;
   let isEven = amari == 0;
   console.log(`${matches}%3 = ${amari}: ${isEven}`);
@@ -17,7 +35,47 @@ function actionAgainstOmoteUra2(){
   return GU;
 }
 
-function action(oppornent){
- let result = actionAgainstOmoteUra2();
- return result;
+//　順番に出していくマンに対するアクション
+function actionAgainstReturn(){
+  let amari = matches % 3;
+  let result = GU;
+  const toReturnCHOKI = amari == 0;
+  const toReturnPA = amari == 1;
+
+  if (toReturnCHOKI){
+    result = CHOKI;
+  } else if(toReturnPA){
+    result = PA;
+  }
+  
+  return result;
 }
+
+/*
+対戦相手名前まとめ
+
+チョキ大好きマン：figther::choli-lover
+表か裏かマン：fighter::odd-even
+帰ってきた表か裏かマン：fighter::on-third
+順番に出していくマン：fighter::rotation
+*/
+
+
+function action(oppornent){
+  console.log(oppornent);
+  countup();
+  if(oppornent == "fighter::choki-lover"){
+    result = actionAgainstChokiLove();
+  } 
+  else if(oppornent == "fighter::odd-even") {
+    result = actionAgainstOmoteUra();
+  }
+  else if(oppornent == "fighter::on-third"){
+    result = actionAgainstOmoteUra2();
+  }
+  else{
+    result = actionAgainstReturn();
+  }
+  return result;
+}
+
